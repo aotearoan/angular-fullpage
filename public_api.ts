8456dcdde40856ce56ab3780b5fb22cbed562701
aotearoan <andrew@aotearoan.com>
2018-12-12 18:11:22 +0100

    add scroll event service to public API

--- public_api.ts
+++ public_api.ts
@@ -1,4 +1,5 @@
 export * from './src/app/modules/fullpage/scroll-event.listener';
+export * from './src/app/modules/fullpage/scroll-event.service';
 export * from './src/app/modules/fullpage/section.model';
 export * from './src/app/modules/fullpage/fullpage.module';
 export * from './src/app/modules/window-ref/window-ref.service';
