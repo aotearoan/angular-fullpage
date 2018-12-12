536b80d4b8d98f20b0c6cbf4cf8dad30f029d625
aotearoan <andrew@aotearoan.com>
2018-12-12 17:57:40 +0100

    add scroll listener & section model to public API

--- public_api.ts
+++ public_api.ts
@@ -1,2 +1,4 @@
+export * from './src/app/modules/fullpage/scroll-event.listener';
+export * from './src/app/modules/fullpage/section.model';
 export * from './src/app/modules/fullpage/fullpage.module';
 export * from './src/app/modules/window-ref/window-ref.service';
